abc1f90d24bec1bb5c02e4e05fd25124f7ba5a96
Nguyễn Đinh Quang Khánh <20127530@student.hcmus.edu.vn>
2022-11-07 17:57:08 +0700

    build: add seed fata

--- backend/prisma/seed/main.js
+++ backend/prisma/seed/main.js
@@ -1,6 +1,12 @@
+/* eslint-disable camelcase */
 /* eslint-disable no-console */
 import { PrismaClient } from '@prisma/client';
-// import leaderboard_record from './leaderboard_record.json';
+import bo_record from './bus_operators.json';
+import bs_record from './bus_stations.json';
+import buses_record from './buses.json';
+import reviews_record from './reviews.json';
+import bt_record from './bus_tickets.json';
+
 const prisma = new PrismaClient();
 
 async function main() {
@@ -9,13 +15,41 @@ async function main() {
       {
         id: 'c118f693-8722-4461-a79d-d76991b96a9e',
         email: 'nguyenphucbao68@gmail.com',
+        role: 0,
+        password: Buffer.from('$2a$10$uR5S.P86tXoBfCHl0a03bePKyN/1yE/1oCW5oRNs/IYfbDeL.WY9O'),
+        create_time: '2022-03-22T12:26:44.480Z',
+        update_time: '2022-08-27T14:43:55.772Z',
+      },
+      {
+        id: 'c118f693-8722-4461-a79d-d76991b96bcd',
+        email: 'khanhndq2002@gmail.com',
+        role: 0,
+        password: Buffer.from('$2a$10$uR5S.P86tXoBfCHl0a03bePKyN/1yE/1oCW5oRNs/IYfbDeL.WY9O'),
+        create_time: '2022-03-22T12:26:44.480Z',
+        update_time: '2022-08-27T14:43:55.772Z',
+      },
+      {
+        id: 'c118f693-8722-4461-a79d-d76991b96sfs',
+        email: 'user1@gmail.com',
+        role: 1,
+        password: Buffer.from('$2a$10$uR5S.P86tXoBfCHl0a03bePKyN/1yE/1oCW5oRNs/IYfbDeL.WY9O'),
+        create_time: '2022-03-22T12:26:44.480Z',
+        update_time: '2022-08-27T14:43:55.772Z',
+      },
+      {
+        id: 'c118f693-8722-4461-a79d-d76991b96hgf',
+        email: 'user2@gmail.com',
         role: 1,
         password: Buffer.from('$2a$10$uR5S.P86tXoBfCHl0a03bePKyN/1yE/1oCW5oRNs/IYfbDeL.WY9O'),
         create_time: '2022-03-22T12:26:44.480Z',
         update_time: '2022-08-27T14:43:55.772Z',
       },
     ],
-    // leaderboard_record,
+    bo_record,
+    bs_record,
+    buses_record,
+    reviews_record,
+    bt_record,
   };
 
   // eslint-disable-next-line no-restricted-syntax
