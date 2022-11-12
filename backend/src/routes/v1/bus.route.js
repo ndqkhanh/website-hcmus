d1cff47f1388acb2038ab46a17b0a959fae1f1df
jacknguyen <nguyenphucbao098@gmail.com>
2022-11-12 16:11:00 +0700

    bus

--- backend/src/routes/v1/bus.route.js
+++ backend/src/routes/v1/bus.route.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const auth = require('../../middlewares/auth');
 const validate = require('../../middlewares/validate');
 const busValidation = require('../../validations/bus.validation');
 const busController = require('../../controllers/bus.controller');
