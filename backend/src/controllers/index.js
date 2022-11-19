a83536a0027d17fc64dfac1018381208c37c7dba
bunrealcua <voduynhan.123@gmail.com>
2022-11-19 16:37:22 +0700

    search buses

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -3,3 +3,4 @@ module.exports.userController = require('./user.controller');
 module.exports.busController = require('./bus.controller');
 module.exports.boController = require('./bo.controller');
 module.exports.ticketController = require('./ticket.controller');
+module.exports.bsController = require('./bs.controller');
