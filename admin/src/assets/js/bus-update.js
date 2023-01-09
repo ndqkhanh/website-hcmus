94f0feaed3f183cac99c741306904174b675e1e7
bunrealcua <voduynhan.123@gmail.com>
2023-01-09 12:07:12 +0700

    fix: fix image url update bus admin

--- admin/src/assets/js/bus-update.js
+++ admin/src/assets/js/bus-update.js
@@ -3,45 +3,46 @@ $(document).ready(function () {
   const OFFSET = 0;
 
   const urlParams = new URLSearchParams(window.location.search);
-  const id = urlParams.get('id');
+  const id = urlParams.get("id");
 
-  let userInfo = localStorage.getItem('userInfo');
+  let userInfo = localStorage.getItem("userInfo");
   userInfo = JSON.parse(userInfo);
   let token = userInfo.token.token;
 
   let typeOfBusList = [
-    { value: 0, type: 'Limousine' },
-    { value: 1, type: 'Normal Seat' },
-    { value: 2, type: 'Sleeper Bus' },
+    { value: 0, type: "Limousine" },
+    { value: 1, type: "Normal Seat" },
+    { value: 2, type: "Sleeper Bus" },
   ];
 
   $.ajax(`${HOST_NAME}/v1/admin/bus/${id}`, {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-      Authorization: 'Bearer ' + token,
+      "Content-Type": "application/json;charset=utf-8",
+      Authorization: "Bearer " + token,
     },
     success: function (bus) {
-      $('#Bus-Operator').html(
+      $("#Bus-Operator").html(
         `<option value = ${bus.bo_id}>${bus.bus_operators.name}</option>`
       );
-      $('#Start-Point').html(
+      $("#Start-Point").html(
         `<option value = ${bus.start_point}>${bus.bus_stations_bus_stationsTobuses_start_point.name}</option>`
       );
-      $('#End-Point').html(
+      $("#End-Point").html(
         `<option value = ${bus.end_point}>${bus.bus_stations_bus_stationsTobuses_end_point.name}</option>`
       );
-      let busType = '';
+      let busType = "";
       typeOfBusList.forEach((item) => {
         if (item.value == bus.type) busType = item.type;
       });
 
-      $('#Type').html(`<option value = ${bus.type}>${busType} </option>`);
-      $('#Start-Time').val(bus.start_time);
-      $('#End-Time').val(bus.end_time);
-      $('#Policy').val(bus.policy);
-      $('#Number-Of-Seats').val(bus.num_of_seats);
-      $('#Price').val(bus.price);
+      $("#Type").html(`<option value = ${bus.type}>${busType} </option>`);
+      $("#Start-Time").val(bus.start_time);
+      $("#End-Time").val(bus.end_time);
+      $("#Policy").val(bus.policy);
+      $("#Number-Of-Seats").val(bus.num_of_seats);
+      $("#Price").val(bus.price);
+      $("#Image-Bus").val(bus.image_url);
     },
     error: function (error) {
       console.log(error);
@@ -52,23 +53,23 @@ $(document).ready(function () {
    * Show list bus operator from db
    */
 
-  $('#Bus-Operator').click(function () {
+  $("#Bus-Operator").click(function () {
     $.ajax(`${HOST_NAME}/v1/bus-operator/list/${OFFSET}/${MAX_LIMIT}`, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-        Authorization: 'Bearer ' + token,
+        "Content-Type": "application/json;charset=utf-8",
+        Authorization: "Bearer " + token,
       },
       success: function (data) {
         let boList = data.data;
-        console.log('bosList', boList);
+        console.log("bosList", boList);
         let boListString = `<select class="form-select" id="Bus-Operator">`;
         boList.forEach((item) => {
           if (userInfo.user.role === 1 && item.id != userInfo.user.boid) return;
           boListString += `<option value=${item.id}>${item.name}</option>`;
         });
-        boListString += '</select>';
-        $('#Bus-Operator').replaceWith(boListString);
+        boListString += "</select>";
+        $("#Bus-Operator").replaceWith(boListString);
       },
       error: function (error) {
         console.log(error);
@@ -79,12 +80,12 @@ $(document).ready(function () {
   /**
    * Show list of Start Point from db
    */
-  $('#Start-Point').click(function () {
+  $("#Start-Point").click(function () {
     $.ajax(`${HOST_NAME}/v1/bus-station/list`, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-        Authorization: 'Bearer ' + token,
+        "Content-Type": "application/json;charset=utf-8",
+        Authorization: "Bearer " + token,
       },
       success: function (data) {
         let startPointList = data.data;
@@ -92,9 +93,9 @@ $(document).ready(function () {
         startPointList.forEach((startPoint) => {
           startPointListHtml += `<option value=${startPoint.id}>${startPoint.name}, ${startPoint.location}</option>`;
         });
-        startPointListHtml += '</select>';
+        startPointListHtml += "</select>";
 
-        $('#Start-Point').replaceWith(startPointListHtml);
+        $("#Start-Point").replaceWith(startPointListHtml);
       },
     });
   });
@@ -102,12 +103,12 @@ $(document).ready(function () {
   /**
    * Show End Point List from DB
    */
-  $('#End-Point').click(function () {
+  $("#End-Point").click(function () {
     $.ajax(`${HOST_NAME}/v1/bus-station/list`, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-        Authorization: 'Bearer ' + token,
+        "Content-Type": "application/json;charset=utf-8",
+        Authorization: "Bearer " + token,
       },
       success: function (data) {
         let endPointList = data.data;
@@ -115,8 +116,8 @@ $(document).ready(function () {
         endPointList.forEach((endPoint) => {
           endPointListHtml += `<option value=${endPoint.id}>${endPoint.name}, ${endPoint.location}</option>`;
         });
-        endPointListHtml += '</select>';
-        $('#End-Point').replaceWith(endPointListHtml);
+        endPointListHtml += "</select>";
+        $("#End-Point").replaceWith(endPointListHtml);
       },
     });
     // let endPointList = await fetch(
@@ -143,8 +144,8 @@ $(document).ready(function () {
    * Show list type of bus from DB
    */
 
-  $('#Type').click(function () {
-    $('#Type').replaceWith(`<select class="form-select" id="Type">
+  $("#Type").click(function () {
+    $("#Type").replaceWith(`<select class="form-select" id="Type">
     <option selected>Select Type</option>
     <option value="0">Limousine</option>
     <option value="1">Normal Seat</option>
@@ -156,21 +157,21 @@ $(document).ready(function () {
    * Update bus
    */
 
-  $('#Update-Button').click(function (e) {
+  $("#Update-Button").click(function (e) {
     e.preventDefault();
 
-    console.log('Hello');
+    console.log("Hello");
     const busId = id;
-    const bo_id = $('#Bus-Operator').val();
-    const start_point = $('#Start-Point').val();
-    const end_point = $('#End-Point').val();
-    const type = $('#Type').val();
-    const start_time = $('#Start-Time').val();
-    const end_time = $('#End-Time').val();
-    const image_url = $('#Image-Bus').val();
-    const policy = $('#Policy').val();
-    const num_of_seats = $('#Number-Of-Seats').val();
-    const price = $('#Price').val();
+    const bo_id = $("#Bus-Operator").val();
+    const start_point = $("#Start-Point").val();
+    const end_point = $("#End-Point").val();
+    const type = $("#Type").val();
+    const start_time = $("#Start-Time").val();
+    const end_time = $("#End-Time").val();
+    const image_url = $("#Image-Bus").val();
+    const policy = $("#Policy").val();
+    const num_of_seats = $("#Number-Of-Seats").val();
+    const price = $("#Price").val();
 
     const data = JSON.stringify({
       bo_id: bo_id,
@@ -186,36 +187,36 @@ $(document).ready(function () {
     });
 
     $.ajax(`${HOST_NAME}/v1/admin/bus/update/${id}`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-        Authorization: 'Bearer ' + token,
+        "Content-Type": "application/json;charset=utf-8",
+        Authorization: "Bearer " + token,
       },
       data: data,
       success: function (data) {
-        console.log('data', data);
-        window.location.href = '/pages/bus';
+        console.log("data", data);
+        window.location.href = "/pages/bus";
       },
       error: function (error) {
-        console.log('error', error);
+        console.log("error", error);
       },
     });
   });
 
-  $('#Delete-Button').click(function (e) {
+  $("#Delete-Button").click(function (e) {
     e.preventDefault();
     $.ajax(`${HOST_NAME}/v1/admin/bus/delete/${id}`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-        Authorization: 'Bearer ' + token,
+        "Content-Type": "application/json;charset=utf-8",
+        Authorization: "Bearer " + token,
       },
       success: function (data) {
-        console.log('data', data);
-        window.location.href = '/pages/bus';
+        console.log("data", data);
+        window.location.href = "/pages/bus";
       },
       error: function (error) {
-        console.log('error', error);
+        console.log("error", error);
       },
     });
   });
